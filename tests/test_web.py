88b1d3499750de94d4d24e017ec5517c6bc81c0a
Thomas Thomassen <thomas@thomthom.net>
2025-01-04 22:49:43 +0100

    Add more notes to flask-login issues.

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,6 +1,8 @@
 import pytest
+from sqlalchemy import select
 
 from app import db
+from app.models import User
 from tests.conftest import authenticated_user, create_user
 
 
@@ -27,7 +29,25 @@ class TestWebRoutes:
             test_user = create_user('test@example.com', 'password123')
             db.session.add(test_user)
             db.session.commit()
+        # The flask-login documentation describe this pattern to authenticate
+        # a user in tests.
         # https://flask-login.readthedocs.io/en/latest/#automated-testing
+        #
+        # However, calling `app.test_client(user=user)` outside of an app context
+        # will lead to an error:
+        #
+        #   sqlalchemy.orm.exc.DetachedInstanceError: Instance <User at 0x20280845e90> is not bound
+        #   to a Session; attribute refresh operation cannot proceed
+        #
+        # And wrapping it in a `with app.app_context():` of its own will not work
+        # either. It only works if the user is creates/or fetched from within the
+        # same app context.
+        #
+        # This is problematic as it directly contradict what flask-sqlalchemy-lite suggests:
+        # https://flask-sqlalchemy-lite.readthedocs.io/en/latest/testing/#testing-data-around-requests
+        # > Accessing db.session or db.engine requires an app context, so you can
+        # > push one temporarily. _Do not make requests inside an active context_,
+        # > they will behave unexpectedly.
         with app.test_client(user=test_user) as client:
             response = client.get("/profile")
             assert response.status_code == 200
